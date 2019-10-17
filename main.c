6f52ba88bf4f77befe84a668c77e4a44bc9b72f4
GalileoFigaro20 <56368433+GalileoFigaro20@users.noreply.github.com>
2019-10-17 11:22:03 +0300

    This is the chatbot's first commitment

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    char s[20];
+    printf("%s", "Let's talk \n");
+    gets(s);
+    printf("%s%s", s, " to you too. What's your name? \n");
+    gets(s);
+    printf("%s%s%s", "It's a pleasure to meet you, ", s, ". Are you a boy or a girl? \n");
+    gets(s);
 }
