b69d718aa9ec1e55559563b950609e1fdc135873
GalileoFigaro20 <56368433+GalileoFigaro20@users.noreply.github.com>
2019-10-17 12:34:04 +0300

    The chatbot project : commit no.1.

--- main.c
+++ main.c
@@ -2,10 +2,24 @@
 
 int main() {
     char s[20];
-    printf("%s", "Let's talk \n");
+    printf("Let's talk \n");
     gets(s);
-    printf("%s%s", s, " to you too. What's your name? \n");
+    printf("%s to you too! I am HAL, what is your first name? (one word, please) \n", s);
     gets(s);
-    printf("%s%s%s", "It's a pleasure to meet you, ", s, ". Are you a boy or a girl? \n");
+    printf("Oh, Hi %s. How old are you?\n", s);
+    int age;
+    scanf("%d", &age);
+    if(age <= 17)
+    {
+        printf("Oh, I'm so sorry you're not allowed to drink at your age. Say a funny thing.\n");
+    }else if(age >= 18)
+    {
+        printf("So, being able to do anything you want is pretty fabulous. Say a funny thing.\n");
+    }
+    int c;
+    while((c= getchar()) != '\n' && c != EOF);
+    gets(s);
+    printf("I'm sorry, but the processor needs to see me. By the way, I don't even know what "
+           "'%s' actually means. Bye, hooman. \n", s);
     gets(s);
 }
