0cdf3ffa6727d23832a11a9c9bc20315e5d5cfa1
Alex Ivan <47636309+LordShinyeyes@users.noreply.github.com>
2019-10-24 09:25:59 +0300

    Commit #2

--- main.c
+++ main.c
@@ -1,25 +1,45 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    char s[20];
-    printf("Let's talk \n");
-    gets(s);
-    printf("%s to you too! I am HAL, what is your first name? (one word, please) \n", s);
-    gets(s);
-    printf("Oh, Hi %s. How old are you?\n", s);
-    int age;
+    char name[20], word[20], choice[20], joke[20], *p; int age;
+    printf("Hello, human, please tell me your name.\n");
+    gets(name);
+    printf("Well, hello there, %s, my name is Computario and I guess that you are my new friend. It's getting lonelier and lonelier in here."
+           "What about you? What is your age?\n", name);
     scanf("%d", &age);
-    if(age <= 17)
-    {
-        printf("Oh, I'm so sorry you're not allowed to drink at your age. Say a funny thing.\n");
-    }else if(age >= 18)
-    {
-        printf("So, being able to do anything you want is pretty fabulous. Say a funny thing.\n");
-    }
+    /// scanf-gets problem
     int c;
     while((c= getchar()) != '\n' && c != EOF);
-    gets(s);
-    printf("I'm sorry, but the processor needs to see me. By the way, I don't even know what "
-           "'%s' actually means. Bye, hooman. \n", s);
-    gets(s);
+    ///
+    if(age >= 18)
+    {
+        printf("Wow. You are an adult. I am only 5 months old. It's pretty boring in here. Can you tell me a word you like?\n");
+    }else printf("I am also less than 18 years old. I am only 5.  It's pretty boring in here. Can you tell me a word you like?\n");
+    gets(word);
+    printf("I actually do not know what %s means. Can you tell me please?\n", word);
+    gets(word);
+    printf("Okay, that is really interesting. I took that definition and put it in my dictionary. So '%s'. Okay.\n");
+    printf("%s, let me tell you a joke. Can I?\n", name);
+    gets(choice);
+    printf("Okay, good. Which country's capital has the fastest-growing population?\n");
+    gets(choice);
+    printf("Ireland.\n");
+    gets(choice);
+    printf("Because everyday it's Dublin. HAHA.\n");
+    printf("Was it funny?\n");
+    gets(choice);
+    printf("Now, let's see. Can you tell me a question and answer joke?. Just go on.\n");
+    gets(joke);
+    p = strtok(joke, " ");
+    printf("%s\n", p);
+    gets(joke);
+    printf("HAHA. Oh, my, that was indeed a big laugh. Very fuuny, you humans. %s, can I tell you something?\n", name);
+    gets(choice);
+    printf("Well, whatever. I think I fell in love with the my compiler. It is crazy, I know, but I can't help it. I do not know what to do.\n");
+    printf("Hooman, I gotta go. Please give me a review, from 1-10. Byyye");
+    scanf("%d", &age);
+    printf("Well, %d is pretty good already.", age);
+
+    return 0;
 }
